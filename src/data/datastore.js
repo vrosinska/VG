f4056365c10d023631419bda4374ad6e834c0a52
19456w <19456w@gmail.com>
2019-07-24 14:28:53 +0200

    add Home, change Footer and navbar Collapse

--- src/data/datastore.js
+++ src/data/datastore.js
@@ -1,4 +1,11 @@
 export const logo =
     {
-               logoImage: '../../../public/images/S-logo.png',
+        logoImage: '../../../public/images/S-logo.png',
+    }
+
+export const home =
+    {
+        mobile: '../../../public/images/mobileCollage.png',
+        iPad: '../../../public/images/iPadCollage.png',
+        laptop: '../../../public/images/WSCollage.png',
     }
