5133d11c5abd01786c4cb23f1b8de33c05345557
19456w <19456w@gmail.com>
2019-08-18 12:33:25 +0200

    add cart logic + extract cartItem from cartPage; additional formatting & code corrections

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './CheckoutForm.scss'
 import classNames from "classnames";
-
+import App from "../../App";
+import {Link} from "react-router-dom";
+import {formatting} from "../../utils";
 
 class CheckoutForm extends React.Component {
-
 
     render() {
         return (
@@ -12,25 +13,38 @@ class CheckoutForm extends React.Component {
                 <a id="checkout"/>
                 <h4>Checkout</h4>
 
-                <div className="row mb-4 py-4 mx-0">
-                    <form className={styles.contactForm} action="https://formspree.io/19456w@gmail.com" method="POST">
+                <div className={classNames("row", styles.formCont)}>
+                    <form action="https://formspree.io/19456w@gmail.com" method="POST">
                         <div className="row">
                             <div className={classNames("col-lg-6 col-sm-12", styles.form)}>
-                                <p><input className="form-control" name="name" placeholder="Your first name and surname" required type="text"/>
+                                <p>
+                                    <input className="form-control" name="name"
+                                           placeholder="Your first name and surname"
+                                           required type="text"/>
                                 </p>
-                                <p><input className="form-control" name="address" placeholder="Your full address" required type="text"/></p>
-                                <p><input className="form-control" name="email" placeholder="Your e-mail" required type="email"/></p>
-                                <p><input className="form-control" name="phone" placeholder="Your phone number" type="numeric"/></p>
+                                <p>
+                                    <input className="form-control" name="address" placeholder="Your full address"
+                                           required type="text"/>
+                                </p>
+                                <p>
+                                    <input className="form-control" name="email" placeholder="Your e-mail" required
+                                           type="email"/>
+                                </p>
+                                <p>
+                                    <input className="form-control" name="phone" placeholder="Your phone number"
+                                           type="numeric"/>
+                                </p>
                             </div>
                             <div className={classNames("col-lg-6 col-sm-12", styles.form)}>
-                                <p><textarea className="form-control" name="text" rows="7" cols="50" placeholder="Please type in your comments here, if needed"
-                                             />
+                                <p>
+                                    <textarea className="form-control" name="text" rows="7" cols="50"
+                                              placeholder="Please type in your comments here, if needed"/>
                                 </p>
                                 <div className={classNames("row", styles.cartSumUp)}>
-                                    <p className={styles.total}>Total: <span>177.16</span>$</p>
+                                    <p>Total: <span>${formatting(App.getInstance().state.totalPrice)}</span></p>
                                 </div>
                                 <div className={classNames("row", styles.checkout)}>
-                                    <a href="#thanks" className={styles.btn}>Checkout</a>
+                                    <Link to="thanks" className={styles.btn}>Checkout</Link>
                                 </div>
                             </div>
                         </div>
